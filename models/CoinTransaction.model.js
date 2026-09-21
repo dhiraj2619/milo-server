db90762ae1dfc634e1518485a2bf94d7d00ea48e
dhiraj <dhirajmymodak22@gmail.com>
2026-09-21 17:04:11 +0530

    some updates

--- models/CoinTransaction.model.js
+++ models/CoinTransaction.model.js
@@ -1,15 +1,62 @@
-const mongoose = require("mongoose");
+const { default: mongoose } = require("mongoose");
 
 const coinTransactionSchema = new mongoose.Schema(
   {
-    user: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, index: true},
-    type: {type: String, required: true, enum: ["welcome_bonus", "daily_claim", "referral_friend_bonus", "referrer_signup_bonus", "coin_purchase", "referrer_first_purchase_bonus"]},
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      index: true,
+    },
+
+    direction: {
+      type: String,
+      enum: ['credit', 'debit'],
+      required: true,
+    },
+
+    type: {
+      type: String,
+      enum: [
+        'welcome_bonus',
+        'daily_claim',
+        'referral_bonus',
+        'coin_purchase',
+        'audio_call_charge',
+        'video_call_charge',
+        'chat_charge',
+        'subscription_purchase',
+        'admin_adjustment',
+        'refund',
+      ],
+      required: true,
+    },
+
     amount: {type: Number, required: true, min: 1},
-    idempotencyKey: {type: String, required: true, unique: true},
-    referenceUser: {type: mongoose.Schema.Types.ObjectId, ref: "User", default: null},
-    paymentId: {type: String, default: null, trim: true},
+
+    balanceAfter: {type: Number, required: true, min: 0},
+
+    idempotencyKey: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    paymentOrder: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'PaymentOrder',
+      default: null,
+    },
+
+    usageEvent: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'UsageEvent',
+      default: null,
+    },
+
+    metadata: {type: mongoose.Schema.Types.Mixed, default: {}},
   },
   {timestamps: true},
 );
 
-module.exports = mongoose.model("CoinTransaction", coinTransactionSchema);
+module.exports = mongoose.model("CoinTransaction",coinTransactionSchema)
