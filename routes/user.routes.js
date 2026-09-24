07e27d9d4925a18ee1e3c44161f49f313fea0e50
dhiraj <dhirajmymodak22@gmail.com>
2026-09-24 15:46:24 +0530

    some updates

--- routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-﻿const router = require("express").Router();
-const {saveUser, getMyProfile, updateMyProfile, getDiscoverProfiles, getLanguages} = require("../controllers/user.controller");
-router.post("/profile", saveUser);
-router.get("/me", getMyProfile);
-router.patch("/me", updateMyProfile);
-router.get("/discover",getDiscoverProfiles);
-router.get('/languages',getLanguages);
-
-module.exports = router;
